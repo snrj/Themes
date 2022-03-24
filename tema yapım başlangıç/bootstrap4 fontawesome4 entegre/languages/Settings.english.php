3c9e6ee823076acd258d05693a19c666eb4ceb98
osman <snrj@users.noreply.github.com>
2022-03-24 19:10:36 +0300

    up

---
+++ tema yapım başlangıç/bootstrap4 fontawesome4 entegre/languages/Settings.english.php
@@ -0,0 +1,16 @@
+<?php
+// Version: 2.1.0; Settings
+ /**
+ * @package isim
+ * @version 1.0
+ * @theme isim
+ * @author Snrj and cee山 - https://smf.web.tr
+ * Copyright 2022 isim
+ *
+ */
+global $settings;
+
+// argument(s): images_url as saved in settings
+$txt['theme_thumbnail_href'] = '%1$s/thumbnail.png';
+$txt['theme_description'] = 'Theme Designer <br /><br />smf.web.tr <a href="https://smf.web.tr/">smf.web.tr</a>';
+?>
---
+++ tema yapım başlangıç/renkler ayrı color css/languages/ThemeStrings.turkish.php
@@ -0,0 +1,13 @@
+<?php
+ /**
+ * @package isim
+ * @version 1.0
+ * @theme isim
+ * @author Snrj and cee山 - https://smf.web.tr
+ * Copyright 2022 isim
+ *
+ */
+
+$txt['themecop']='<span class="smalltext"><span class="main_icons current_theme"></span> Theme Designer <a href="https://smf.web.tr" title="smf theme" target="_blank">Smf.web.tr</a></span>';
+
+?>
